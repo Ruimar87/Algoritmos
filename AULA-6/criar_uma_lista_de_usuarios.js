72ae5b33398d937fd7c18f78ac3a44e6459c2859
Ruimar  Silvano <ruimarsilvano2@gmail.com>
2024-04-29 19:36:43 -0300

    feat criar_uma_lista_de_usuarios_implementada.js

---
+++ AULA-6/criar_uma_lista_de_usuarios.js
@@ -0,0 +1,15 @@
+// 1 - Necessito criar um lista de usuários com os nomes
+// Sofia
+// Lucas
+// Isabella
+// Mateus
+// Alice
+// 
+// Essa lista deve ser armazenada em uma constante.
+// Quero que na tela seja impresso na saída os nomes, Sofia, Isabella e Alice.
+// Dica: Será necessário utilizar um recurso chamado ARRAY
+
+const usuarios = ["Sofia", "Lucas", "Isabella", "Mateus", "Alice" ];
+console.log(usuarios [0] , usuarios[2], usuarios[4]);
+
+
